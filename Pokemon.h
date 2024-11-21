6c48ddf9fe10d0360162d9288f3ee0a2e532ede7
jack <144387193+failedsyntax@users.noreply.github.com>
2024-11-20 19:00:24 -0600

    Pokemon Class completed

--- Pokemon.h
+++ Pokemon.h
@@ -1,12 +1,53 @@
-#ifndef DATA_H
-#define DATA_H
+#ifndef POKEMON_H
+#define POKEMON_H
 
 #include <iostream>
 
 using namespace std;
 
-class Data {
-    
+class Pokemon {
+    private:
+        int Index;
+        string Name;
+
+    public:
+        Pokemon() {
+            Index = 0;
+            Name = "None";
+        };
+
+        Pokemon(int newIndex, string newName) {
+            Index = newIndex;
+            Name = newName;
+        };
+
+        void setID(int newID) {
+            Index = newID;
+        }
+
+        void setName(string newName) {
+            Name = newName;
+        };
+
+        int getID() {
+            return Index;
+        };
+
+        string getName() {
+            return Name;
+        };
+
+        friend ostream & operator<< (ostream& os, Pokemon Item) {
+            os << "Pokemon ID: " << Item.getID() << "      |      Name: " << Item.getName() << endl;
+        }
+
+        friend bool operator== (const Pokemon &Left, const Pokemon &Right) {
+            return Left.Index == Right.Index;
+        }
+
+        friend bool operator< (const Pokemon &Left, const Pokemon &Right) {
+            return Left.Index < Right.Index;
+        }
 };
 
 #endif
---
+++ driver.cpp
@@ -0,0 +1,9 @@
+#include "BinaryTree.h"
+#include "Pokemon.h"
+
+int main() {
+
+    
+
+    return 0;
+}
